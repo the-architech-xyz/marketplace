754dbd8b085d8bb6da3bd31ae61572deb1dbd4ef
Antoinesrvt <antoine.srvt@gmail.com>
2025-10-21 02:35:22 +0200

    types regenerated

--- types/blueprint-config-types.js
+++ types/blueprint-config-types.js
@@ -3,14 +3,10 @@
  */
 
 export function extractTypedModuleParameters(config) {
-  // CRITICAL FIX: Extract from MergedConfiguration.templateContext structure
-  // The config passed to blueprints has parameters nested in templateContext.module.parameters
-  const moduleParams = config.templateContext?.module?.parameters || {};
-  
   return {
-    params: moduleParams,
-    features: moduleParams.features || {},
-    framework: config.framework || config.templateContext?.project?.framework,
-    paths: config.paths || config.templateContext?.paths || {},
+    params: config.parameters || {},
+    features: config.features || {},
+    framework: config.framework,
+    paths: config.paths || {},
   };
 }
